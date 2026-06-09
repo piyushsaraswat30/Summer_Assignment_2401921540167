6b4ce0192de6e9171b3cb7d7dde479650bc4e48e
Piyush Saraswat <saraswatpiyush789@gmail.com>
2026-06-09 13:39:19 +0530

    Week 2 Day 2 Completed

---
+++ Week_2/Day_2/PermutationInString.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+    if (s1.length() > s2.length())
+    return false;
+
+    int[] count1 = new int[26];
+    int[] count2 = new int[26];
+
+    for(char c:s1.toCharArray()) {
+        count1[c - 'a']++;
+    }    
+
+    int windowSize = s1.length();
+
+    for(int i =0; i<s2.length(); i++) {
+
+        count2[s2.charAt(i) - 'a']++;
+        if( i>= windowSize) {
+            count2[s2.charAt(i - windowSize) - 'a']--;
+        }
+        if(java.util.Arrays.equals(count1, count2)) {
+            return true;
+        }
+    }
+    return false;
+    }
+}
