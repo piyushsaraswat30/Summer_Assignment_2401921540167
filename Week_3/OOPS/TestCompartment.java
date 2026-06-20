429c44134f92792a39441886a7f184ea197e2c2b
Piyush Saraswat <saraswatpiyush789@gmail.com>
2026-06-20 22:42:45 +0530

    Week-3, OOPS Problem Completed

---
+++ Week_3/OOPS/TestCompartment.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+abstract class Compartment {
+    public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+    public String notice() {
+        return "First Class Compartment";
+    }
+}
+
+class Ladies extends Compartment {
+    public String notice() {
+        return "Ladies Compartment";
+    }
+}
+
+class General extends Compartment {
+    public String notice() {
+        return "General Compartment";
+    }
+}
+
+class Luggage extends Compartment {
+    public String notice() {
+        return "Luggage Compartment";
+    }
+}
+
+public class TestCompartment {
+
+    public static void main(String[] args) {
+
+        Compartment[] compartments = new Compartment[10];
+        Random random = new Random();
+
+        for(int i = 0; i < compartments.length; i++) {
+
+            int n = random.nextInt(4) + 1;
+
+            switch(n) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+
+                case 3:
+                    compartments[i] = new General();
+                    break;
+
+                default:
+                    compartments[i] = new Luggage();
+            }
+        }
+
+        for(Compartment c : compartments) {
+            System.out.println(c.notice());
+        }
+    }
+}
