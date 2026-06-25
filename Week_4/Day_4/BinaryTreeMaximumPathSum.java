bcfb88713b2265fa576e0b03b903a9bd534407c9
Piyush Saraswat <saraswatpiyush789@gmail.com>
2026-06-25 13:33:14 +0530

    Week-4, Day-4 Problems Completed

---
+++ Week_4/Day_4/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+       return maxSum; 
+    }
+
+    private int dfs(TreeNode node) {
+
+        if (node == null)
+            return 0;
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+
+        maxSum = Math.max(maxSum, left + right + node.val);
+
+        return Math.max(left, right) + node.val;
+    }
+}
